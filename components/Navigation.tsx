2bde4d7d5b8d6c6fc14868952f56577d25f1afab
Tobias Lilja <lilja.tobias94@gmail.com>
2022-02-22 18:07:12 +0100

    feat: added left side Drawer

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,6 +1,8 @@
 import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import Link from 'next/link'
+import NavDrawer from './NavDrawer'
+import { useCallback, useState } from 'react'
 
 interface NavigationProps {
   info: {
@@ -13,20 +15,32 @@ interface NavigationProps {
 }
 
 const Navigation: React.FC<NavigationProps> = ({ info, children }) => {
+  const [isNavDrawerOpen, setIsNavDrawerOpen] = useState(false)
   const { name, logo_url, theme } = info
+
+  const handleNavDrawToggle = useCallback(
+    (open = !isNavDrawerOpen) => {
+      setIsNavDrawerOpen(open)
+    },
+    [isNavDrawerOpen],
+  )
+
   return (
-    <AppBar position="static" color="primary" style={{ backgroundColor: theme.primaryColor }}>
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" color="inherit">
-          <Link href="/">{name}</Link>
-        </Typography>
-        <img src={logo_url} alt={name} />
-      </Toolbar>
-      {children}
-    </AppBar>
+    <>
+      <AppBar position="static" color="primary" style={{ backgroundColor: theme.primaryColor }}>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleNavDrawToggle}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            <Link href="/">{name}</Link>
+          </Typography>
+          <img src={logo_url} alt={name} />
+        </Toolbar>
+        {children}
+      </AppBar>
+      <NavDrawer open={isNavDrawerOpen} onClose={handleNavDrawToggle} />
+    </>
   )
 }
 
