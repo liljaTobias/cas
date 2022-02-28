0151a89091ea3d9aec9d840bdc0820329cbabc8e
Tobias Lilja <lilja.tobias94@gmail.com>
2022-02-28 20:24:58 +0100

    feat: login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,4 +1,14 @@
-import { Button, Grid, MenuItem, Select, SelectChangeEvent, Typography } from '@mui/material'
+import {
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  Grid,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from '@mui/material'
 import { NextPage } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
@@ -6,6 +16,11 @@ import { useState } from 'react'
 import useSWR from 'swr'
 import { TOrganizationsItems } from '../../types/api'
 import { fetcher } from '../../utils/api'
+
+import styles from '../../styles/Login.module.css'
+import Image from 'next/image'
+
+import logoImage from '../../public/icon-144x144.png'
 
 interface LoginProps {}
 
@@ -37,29 +52,48 @@ const Login: NextPage<LoginProps> = () => {
   }
 
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>CAS - Login</title>
       </Head>
-      <Grid container alignItems="center" justifyContent="center" direction="column">
-        <Typography variant="h2">Login</Typography>
-        <Typography variant="body1">Välj din organisation</Typography>
-        <Select value={organizationSelectValue} disabled={!data} onChange={handleChange}>
-          {data?.Items.map((item) => (
-            <MenuItem key={item.organization_id} value={item.organization_id}>
-              {item.info ? item.info.name : item.organization_name}
-            </MenuItem>
-          ))}
-          {/* <MenuItem value="kommunkoping_v2">Kommunköping</MenuItem> */}
-        </Select>
-        <Button variant="contained" onClick={() => handleLogin()}>
-          Gå vidare
-        </Button>
-        <Button color="error" onClick={fakeLogin}>
-          DEBUG: LOGGA IN
-        </Button>
-      </Grid>
-    </>
+      <div className={styles.main}>
+        <Grid container direction="column" justifyContent="center" alignItems="center">
+          <Image src={logoImage} alt="Logo image" />
+          <Typography variant="h1" fontSize={32}>
+            Välkommen till CAS
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            Börja med att välja din organisation
+          </Typography>
+          <FormControl fullWidth>
+            <Select
+              autoWidth
+              value={organizationSelectValue}
+              disabled={!data}
+              onChange={handleChange}
+              className={styles.input}
+            >
+              {data?.Items.map((item) => (
+                <MenuItem
+                  key={item.organization_id}
+                  value={item.organization_id}
+                  disabled={item.organization_id !== 'kommunkoping_v2'}
+                >
+                  {item.info ? item.info.name : item.organization_name}
+                </MenuItem>
+              ))}
+              {/* <MenuItem value="kommunkoping_v2">Kommunköping</MenuItem> */}
+            </Select>
+            <Button variant="contained" onClick={() => handleLogin()} className={styles.input}>
+              Gå vidare
+            </Button>
+            <Button variant="contained" color="error" onClick={fakeLogin}>
+              DEBUG: LOGGA IN
+            </Button>
+          </FormControl>
+        </Grid>
+      </div>
+    </div>
   )
 }
 
