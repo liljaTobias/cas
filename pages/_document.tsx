12d317794b156f6d43244471cd7afc99c48e7ac4
Tobias Lilja <lilja.tobias94@gmail.com>
2022-02-28 21:08:49 +0100

    fix: theme colors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,6 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-          <meta name="theme-color" content="#42a5f5" />
         </Head>
         <body>
           <Main />
