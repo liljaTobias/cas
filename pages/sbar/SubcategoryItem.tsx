ad95f98e69407af8b98616ae0b6373df173af603
Tobias Lilja <liljaTobias@users.noreply.github.com>
2022-03-12 17:41:43 +0100

    fix: removed auth for sbar-page
    * fix: removec auth for sbar-page
    
    * fix: removed redirect from index

--- pages/sbar/SubcategoryItem.tsx
+++ pages/sbar/SubcategoryItem.tsx
@@ -1,16 +1,19 @@
-import { Divider, ListItemButton, ListItemText } from '@mui/material'
+import { ExpandLess, ExpandMore } from '@mui/icons-material'
+import { Divider, ListItemButton, ListItemSecondaryAction, ListItemText } from '@mui/material'
 import styles from '../../styles/Sbar.module.css'
 
 interface SubcategoryItemProps {
   name: string
   onClick: () => void
+  isOpen: boolean
 }
 
-const SubcategoryItem: React.FC<SubcategoryItemProps> = ({ name, onClick }) => {
+const SubcategoryItem: React.FC<SubcategoryItemProps> = ({ name, onClick, isOpen }) => {
   return (
     <div className={styles.subcategoryItem}>
       <ListItemButton onClick={onClick}>
         <ListItemText primary={name} primaryTypographyProps={{ sx: { fontWeight: '500' } }} />
+        <ListItemSecondaryAction>{isOpen ? <ExpandLess /> : <ExpandMore />}</ListItemSecondaryAction>
       </ListItemButton>
       <Divider />
     </div>
