323fb1b775a61c91e7ce003f041c15290a3beedb
Tobias Lilja <lilja.tobias94@gmail.com>
2022-04-04 23:47:52 +0200

    feat: email authentication

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,36 @@
 import NextAuth from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
+import EmailProvider from 'next-auth/providers/email'
+
+import { DynamoDB, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import { DynamoDBAdapter } from '@next-auth/dynamodb-adapter'
+
+const config: DynamoDBClientConfig = {
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.AWS_SECRET_KEY as string,
+  },
+  region: process.env.AWS_REGION,
+}
+
+const client = DynamoDBDocument.from(new DynamoDB(config), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  },
+})
 
 export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
     }),
   ],
   callbacks: {
@@ -14,6 +39,7 @@ export default NextAuth({
       return token
     },
   },
+  adapter: DynamoDBAdapter(client),
   // pages: {
   //   signIn: '/auth/signin',
   // },
