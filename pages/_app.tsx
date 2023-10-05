1eacc6792298c2412c9dd5855b6694d32f201ebe
Tobias Lilja <lilja.tobias94@gmail.com>
2023-10-05 12:28:29 +0200

    feat: FooterAd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { appWithTranslation } from 'next-i18next'
 import { Page } from '../types/page'
 import Layout from '../components/Layout'
 import { SessionProvider } from 'next-auth/react'
+import Script from 'next/script'
 
 type AppPropsWithLayout = AppProps & {
   Component: Page
@@ -11,11 +12,23 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <>
+      <Script
+        id="Adsense-id"
+        async
+        strategy="afterInteractive"
+        onError={(e) => {
+          console.error('Script failed to load', e)
+        }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7303190163223512"
+        crossOrigin="anonymous"
+      />
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </>
   )
 }
 
