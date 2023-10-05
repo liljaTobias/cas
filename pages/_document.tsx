fc1daace07913a4cb4e1040b2211ddbbbd61e654
Tobias Lilja <liljaTobias@users.noreply.github.com>
2023-10-05 10:54:19 +0200

    feat: adsense setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <meta name="google-adsense-account" content="ca-pub-7303190163223512"></meta>
         </Head>
         <body>
           <Main />
